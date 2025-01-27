001f4faa4ad9ba300dc0ac2c03ab8cd3b0766b12
P2vel33 <samasyt@mail.ru>
2025-01-27 23:29:38 +0300

    update page Error

--- VITE_REACT/VITE_REACT/src/App.jsx
+++ VITE_REACT/VITE_REACT/src/App.jsx
@@ -5,17 +5,13 @@ import About from "./pages/About";
 import Posts from "./pages/Posts";
 import Navbar from "./components/UI/navbar/Navbar";
 import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="/*" element={<Navigate to="/error" replace />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   );
 }
