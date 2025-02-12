15a16e98b0cfa65c59e65eaf66ed510da9f9dc09
P2vel33 <samasyt@mail.ru>
2025-02-12 19:48:01 +0300

    add router

--- REPETITION_VITE_REACT_/src/App.jsx
+++ REPETITION_VITE_REACT_/src/App.jsx
@@ -1,11 +1,16 @@
 import "./styles/App.css";
 import Posts from "./pages/Posts";
+import { Routes, Route, BrowserRouter } from "react-router";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <Posts />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ REPETITION_VITE_REACT_/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>This page about our project</h2>
+    </div>
+  );
+};
+
+export default About;
