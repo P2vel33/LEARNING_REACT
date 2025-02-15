61a504bca6f499589e46bae509f1ba0d6e5ac2b8
P2vel33 <samasyt@mail.ru>
2025-02-15 12:53:18 +0300

    feat: adding props to a component MyJournalItem

--- PetProject/src/App.jsx
+++ PetProject/src/App.jsx
@@ -6,12 +6,34 @@ import "./App.css";
 import MyJournalItem from "./components/UI/MyJournalItem/MyJournalItem";
 
 function App() {
+  const data = [
+    {
+      title: "First",
+      text: "1",
+      date: new Date(),
+    },
+    {
+      title: "Second",
+      text: "2",
+      date: new Date(),
+    },
+  ];
+
   return (
     <>
       <h1>WaQ</h1>
       <p>Yes</p>
       <MyButton></MyButton>
-      <MyJournalItem />
+      <MyJournalItem
+        title={data[0].title}
+        text={data[0].text}
+        date={data[0].date}
+      />
+      <MyJournalItem
+        title={data[1].title}
+        text={data[1].text}
+        date={data[1].date}
+      />
     </>
   );
 }
--- PetProject/src/components/UI/MyJournalItem/MyJournalItem.jsx
+++ PetProject/src/components/UI/MyJournalItem/MyJournalItem.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import classes from "./MyJournalItem.module.css";
 
-const MyJournalItem = () => {
-  const title = "Learn";
-  const date = new Date();
-  const text = "Text";
-
+const MyJournalItem = (props) => {
   return (
     <div className={classes.journal_item}>
-      <h2 className={classes.journal_item__header}>{title}</h2>
+      <h2 className={classes.journal_item__header}>{props.title}</h2>
       <h2 className={classes.journal_item__body}>
-        <div className={classes.journal_item__date}>{date.toString()}</div>
-        <div className={classes.journal_item__text}>{text}</div>
+        <div className={classes.journal_item__date}>
+          {props.date.toString()}
+        </div>
+        <div className={classes.journal_item__text}>{props.text}</div>
       </h2>
     </div>
   );
