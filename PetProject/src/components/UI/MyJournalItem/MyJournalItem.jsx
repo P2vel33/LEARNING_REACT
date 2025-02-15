49f1f9907e0fe8424b50754e1b14c207ce11aa93
P2vel33 <samasyt@mail.ru>
2025-02-15 13:05:01 +0300

    feat: adding correct date output

--- PetProject/src/components/UI/MyJournalItem/MyJournalItem.jsx
+++ PetProject/src/components/UI/MyJournalItem/MyJournalItem.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import classes from "./MyJournalItem.module.css";
 
 const MyJournalItem = (props) => {
+  const formatedDate = new Intl.DateTimeFormat("ru-RU").format(props.date);
+
   return (
     <div className={classes.journal_item}>
       <h2 className={classes.journal_item__header}>{props.title}</h2>
       <h2 className={classes.journal_item__body}>
-        <div className={classes.journal_item__date}>
-          {props.date.toString()}
-        </div>
+        <div className={classes.journal_item__date}>{formatedDate}</div>
         <div className={classes.journal_item__text}>{props.text}</div>
       </h2>
     </div>
