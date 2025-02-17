f4ea80ce6c52dd849322a32f9d066f538977a312
P2vel33 <samasyt@mail.ru>
2025-02-17 22:18:44 +0300

    feat: adding event liistener on MyButton component

--- PetProject/src/components/UI/MyButton/MyButton.jsx
+++ PetProject/src/components/UI/MyButton/MyButton.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import classes from "./MyButton.module.css";
 const MyButton = () => {
+  const clicked = () => {
+    console.log("Hi");
+  };
+
   return (
-    <button className={`${classes.button} ${classes.accent}`}>Save</button>
+    <button onClick={clicked} className={`${classes.button} ${classes.accent}`}>
+      Save
+    </button>
   );
 };
 
